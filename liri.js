b4731dc5877a44edfbde9a4e72ed0bc6c2f84eed
Blake Robert <Blaker.blaker4@gmail.com>
2018-09-08 12:47:22 -0400

    all requiremants met

--- liri.js
+++ liri.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 var keys = require('./keys.js');
+var fs = require('fs');
 var request = require('request');
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
@@ -10,52 +11,72 @@ var spotifyuserinput = process.argv.slice(3).join("%20");
 var concertuserinput = process.argv.slice(3).join("");
 
 
-var concert = function(artist){
-    request("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp", function(error, response, body) {
-  if (!error && response.statusCode === 200) {
-      var api = JSON.parse(body)
-      console.log(`
+var concert = function (artist) {
+    request("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp", function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            var api = JSON.parse(body)
+            console.log(`
       Upcoming concerts for ${artist}.
       `)
-      if(api.length > 0){
-      for(var i = 0; i < body.length && i<2; i++){
-          console.log(`
+            if (api.length > 0) {
+                for (var i = 0; i < body.length && i < 2; i++) {
+                    console.log(`
     Venue:    ${api[i].venue.name}
     Location: ${api[i].venue.city}
     Date::    ${api[i].datetime}
 
           `)
-      }
-  }else{
-      console.log(`
+                }
+            } else {
+                console.log(`
      ${userinput} has no upcoming shows. :(
     `)
-  }
-}})
+            }
+        }
+    })
 };
-var spot = function(song){
-    spotify.search({ type: 'track', query: song , limit: 5})
-    .then(function(response) {
-        var api = response
-          for(i=0;i<api.tracks.items.length;i++){
-              console.log(`
+var spot = function (song) {
+    if (song) {
+        spotify.search({ type: 'track', query: song, limit: 5 }, function (err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            } else {
+                var api = data
+                for (i = 0; i < api.tracks.items.length; i++) {
+                    console.log(`
     Artist:     ${api.tracks.items[i].album.artists[0].name}
     Song:       ${api.tracks.items[i].name}
     Preview:    ${api.tracks.items[i].preview_url}
     Album:      ${api.tracks.items[i].album.name}
               `)
-          }
+                }
+            }
+        });
 
-      })
-      .catch(function(err){
-          console.log(err)
-      }); 
+    } else {
+        spotify.search({ type: 'track', query: 'The Sign Ace of Base', limit: 1 }, function (err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            } else {
+                var api = data
+                for (i = 0; i < api.tracks.items.length; i++) {
+                    console.log(`
+    Artist:     ${api.tracks.items[i].album.artists[0].name}
+    Song:       ${api.tracks.items[i].name}
+    Preview:    ${api.tracks.items[i].preview_url}
+    Album:      ${api.tracks.items[i].album.name}
+              `)
+                }
+            }
+        });
+
+    }
 };
-var movie = function(title){
-    request("http://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=trilogy", function(error, response, body) {
-        if(userinput){
-    if (!error && response.statusCode === 200) {
-    console.log(`
+var movie = function (title) {
+    request("http://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
+        if (userinput) {
+            if (!error && response.statusCode === 200) {
+                console.log(`
     Title:           ${JSON.parse(body).Title}
     Year:            ${JSON.parse(body).Year}
     IMDB Rating:     ${JSON.parse(body).imdbRating}
@@ -65,12 +86,12 @@ var movie = function(title){
     Plot:            ${JSON.parse(body).Plot}
     Actors:          ${JSON.parse(body).Actors}
     `);
-  }
-}
-else{
-    request("http://www.omdbapi.com/?t=Mr. Nobody&y=&plot=short&apikey=trilogy", function(error, response, body) {
-  if (!error && response.statusCode === 200) {
-    console.log(`
+            }
+        }
+        else {
+            request("http://www.omdbapi.com/?t=Mr. Nobody&y=&plot=short&apikey=trilogy", function (error, response, body) {
+                if (!error && response.statusCode === 200) {
+                    console.log(`
     Title:           ${JSON.parse(body).Title}
     Year:            ${JSON.parse(body).Year}
     IMDB Rating:     ${JSON.parse(body).imdbRating}
@@ -80,11 +101,13 @@ else{
     Plot:            ${JSON.parse(body).Plot}
     Actors:          ${JSON.parse(body).Actors}
     `);
-  }
-})
+                }
+            })
 
-}})};
-var badCmd = function(){
+        }
+    })
+};
+var badCmd = function () {
     console.log(`
     ~~~~~~~~~~~~~~~~~~
     INCORRECT COMMAND!
@@ -96,15 +119,35 @@ var badCmd = function(){
     ~~~~~~~~~~~~~~~~~~
     `)
 }
+var random = function () {
+    
+    fs.readFile("random.txt", "utf8", function (err, data) {
+        if (err) {
+            return console.log(err);
+        }
+       var data = data.replace("\"", "");
+       data = data.replace("\"", "");
+        var words =data.split(',');
+        if (words[0] === "concert-this") {
+            concert(words[1]);
+        } else if (words[0] === "spotify-this-song") {
+            spot(words[1]);
+        } else if (words[0] === "movie-this") {
+            movie(words[1]);
+        } else {
+            badCmd();
+        }
+    })
+};
 
-if(command === "concert-this"){
+if (command === "concert-this") {
     concert(concertuserinput);
-}else if(command === "spotify-this-song"){
+} else if (command === "spotify-this-song") {
     spot(spotifyuserinput);
-}else if(command === "movie-this"){
+} else if (command === "movie-this") {
     movie(userinput);
-}else if(command === "do-what-it-says"){
+} else if (command === "do-what-it-says") {
     random();
-}else{
+} else {
     badCmd();
 }
